b7a1752e3328b36ea5e6ef148ff11be4f1fe47a2
Sanket Arun Mule <sanketmu@cybage.com>
2026-09-02 15:39:51 +0530

    new start

--- environments/production/providers.tf
+++ environments/production/providers.tf
@@ -7,14 +7,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 3.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.30"
-    }
   }
 }
 
